3f5fb81b72b33b753539fa53af8952681a89303d
Braden Durian <braden.durian@csuglobal.edu>
2024-06-30 10:53:56 -0600

    First Commit: created source file

---
+++ CT3.cpp
@@ -0,0 +1,21 @@
+/*
+*CT3.cpp
+* Created by:bhdur
+*Jun 30, 2024
+*10:52:57 AM
+*/
+#include <iostream>
+
+using namespace std;
+//Function Prototype
+
+int main()
+{
+
+//Todo Place code here
+
+return 0;
+}
+
+//Function Definition
+cout << "Eclipse C++"<<endl;
