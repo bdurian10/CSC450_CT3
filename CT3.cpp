259909f7d85a6c5b68324c0fe066703160848e38
Braden Durian <braden.durian@csuglobal.edu>
2024-06-30 11:52:43 -0600

    Second Commit: added logic for handling user input and prototyped printing of pointer values and addresses

--- CT3.cpp
+++ CT3.cpp
@@ -1,21 +1,61 @@
 /*
 *CT3.cpp
-* Created by:bhdur
+* Created by:Braden Durian
 *Jun 30, 2024
 *10:52:57 AM
 */
 #include <iostream>
+#include <limits>
 
 using namespace std;
-//Function Prototype
 
 int main()
 {
 
-//Todo Place code here
+	int num1, num2, num3; //Declare integer variables
+
+	cout << "Enter your first integer" << endl;
+	while(!(cin >> num1)) //extraction and input validation
+	{
+		cout << "Your entry was not a valid integer, please try again" << endl;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+	cin.ignore(numeric_limits<streamsize>::max(), '\n'); //clear any leftover input in the buffer between entries
+
+	cout << "Enter your second integer" << endl;
+	while(!(cin >> num2))
+	{
+		cout << "Your entry was not a valid integer, please try again" << endl;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	cout << "Enter your final integer" << endl;
+	while(!(cin >> num3))
+	{
+		cout << "Your entry was not a valid integer, please try again" << endl;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	//Declare and set pointers
+	int* p_Num1 = new int(num1);
+	int* p_Num2 = new int(num2);
+	int* p_Num3 = new int(num3);
+
+	//Print Integer 1
+	cout << "Integer 1 Value: " << *p_Num1 << endl;
+	cout << "Integer 1 Address: " << p_Num1 << endl;
+	//Print Integer 2
+	cout << "Integer 2 Value: " << *p_Num2 << endl;
+	cout << "Integer 2 Address: " << p_Num2 << endl;
+
+	delete p_Num1;
+	delete p_Num2;
+	delete p_Num3;
 
 return 0;
 }
-
-//Function Definition
-cout << "Eclipse C++"<<endl;
